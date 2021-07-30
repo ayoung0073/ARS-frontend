97603892c43e9c343d4e749529ebf35aa81f8e74
ayoung0073 <ayong0310@naver.com>
2021-07-30 23:11:37 +0900

    Fix : 비동기 제거

--- src/api/get/checkMember.js
+++ src/api/get/checkMember.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const base = require('../../base')
 
-export const checkMember = async () => {
+export function checkMember() {
     const url =
         base.url + '/api/members/check';
 
@@ -15,7 +15,7 @@ export const checkMember = async () => {
     }
 
     try {
-        const response = await axios(option);
+        const response = axios(option);
         console.log('[SUCCESS] GET ', response);
         if (response.data.status !== 200) {
             return false;
